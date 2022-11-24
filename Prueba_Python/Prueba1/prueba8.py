aa19dea44edfd6877305c0bd1bdd4f4fb75df83f
= <=>
2022-11-24 15:17:14 +0100

    he añadido funciones para conseguir info en P9

--- Prueba_Python/Prueba1/prueba8.py
+++ Prueba_Python/Prueba1/prueba8.py
@@ -45,12 +45,13 @@ def login(email, password):
         
         #driver.maximize_window()
         # filling the form
-        driver.find_element_by_id('"u_0_e_14"').click()
-        driver.find_element_by_name('email').send_keys(email)
-        driver.find_element_by_name('pass').send_keys(password)
-        
+
+        driver.find_element(By.CSS_SELECTOR,"._9xo5 ._9xo7").click()
+        driver.find_element(By.XPATH, "//input[@placeholder='Correo electrónico o número de teléfono']").send_keys(email)
+        driver.find_element(By.XPATH, "//input[@placeholder='Contraseña']").send_keys(password)
         # clicking on login button
-        driver.find_element_by_id('u_0_b').click()
+
+        driver.find_element(By.NAME,"login").click()
         
     except:
         print("maybe something is wrong")
@@ -67,15 +68,14 @@ def main():
             print("Your email or password is missing. Kindly write them in credentials.txt")
             exit()
 
-    #ids = ["https://en-gb.facebook.com/" + line.split("/")[-1] for line in open("input.txt", newline='\n')]
-    #ids = ["https://facebook.com/" + line.split("/")[-1] for line in open("input.txt", newline='\n')]
-    ids=["https://facebook.com/"]
+    ids = ["https://facebook.com/" + line.split("/")[-1] for line in open("input.txt", newline='\n')]
     if len(ids) > 0:
         print("\nStarting Scraping...")
         login(email, password)
         time.sleep(5)
         print(ids)
         driver.get(ids[0])
+        print(driver.find_elements(By.CLASS_NAME,"x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x676frb x1nxh6w3 x1sibtaa x1s688f xzsf02u"))
         #driver.close()
     else:
         print("Input file is empty.")
--- Prueba_Python/Prueba1/prueba9.py
+++ Prueba_Python/Prueba1/prueba9.py
@@ -6,6 +6,9 @@ Created on 25 oct 2022
 
 from sys import exit
 import os
+from asyncio.tasks import sleep
+from numpy.core._type_aliases import void
+from _overlapped import NULL
 os.chdir(r"C:\Users\zorro\Desktop\experimentos")
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -43,15 +46,17 @@ def bajaHastaAbajo():
 
 def busca_amigos(driver):
     driver.get(driver.current_url+"/allende.camaracarrasco/friends")
-    #bajaHastaAbajo()
+    bajaHastaAbajo()
     cont=1
-    while(cont<3):
+    while(cont<30):
         nombre=driver.find_element(By.CSS_SELECTOR, ".x6s0dn4:nth-child({}) .x1i10hfl > .x193iq5w".format(cont)).text
         #para conseguir la foto de perfil del amigo
         #foto=driver.find_element(By.CSS_SELECTOR, ".x6s0dn4:nth-child({}) .x1lq5wgf:nth-child(2)".format(cont))
         print(nombre)
         cont=cont+1
-        
+    
+    
+    
 def busca_fotos(driver):
     if not os.path.exists("fotos"):
             os.mkdir("fotos")
@@ -67,7 +72,7 @@ def busca_fotos(driver):
         
 def busca_info(driver):
     
-    driver.get(driver.current_url+"/joaquincasado14/about")
+    driver.get("https://www.facebook.com//allende.camaracarrasco/about")
     print("\n")
     print("Información personal general")
     #donde trabaja
@@ -80,51 +85,106 @@ def busca_info(driver):
     print(driver.find_element(By.CSS_SELECTOR, ".x1hq5gj4:nth-child(4) .xzsf02u > .x193iq5w").text)
     #tiene una relación?
     print(driver.find_element(By.CSS_SELECTOR, ".x12nagc > .xo1l8bm").text)
+
+def info_work_education(driver):
+    time.sleep(2)
+    driver.get("https://www.facebook.com/mario.rubioperez.3/about_work_and_education")
+    print("Trabajo")
+    print(driver.find_element(By.CSS_SELECTOR, ".x1gan7if:nth-child(1) > .xat24cr .x9f619 > .x9f619 > .x193iq5w").text)
+    print("\n")
+    print("Universidades")
+    try:
+        print(driver.find_element(By.CSS_SELECTOR, ".xat24cr:nth-child(2) .xzsf02u > .x193iq5w").text)
+    except:
+        print("No schools to show")
+        
+    print("\n")
+    
+    print("Colegios")
+    try:
+        cont2=2
+        colegios=[]
+        while(cont2<4):
+            if(len(colegios)==0):
+                colegio=driver.find_element(By.CSS_SELECTOR, ".x1hq5gj4 .xzsf02u > .x193iq5w").text
+                colegios.append(colegio)
+            else:
+                colegio=driver.find_element(By.CSS_SELECTOR, ".xat24cr:nth-child({}) .xzsf02u > .x193iq5w".format(cont2)).tex
+                if colegio not in colegios:
+                    colegios.append(colegio)
+            cont2=cont2+1
+        print(colegios)
+    except:
+        print("No schools to show")  
+        
+        
+def info_places_lived(driver):
+    time.sleep(2)
+    driver.get("https://www.facebook.com/joaquincasado14/about_places")
+    print("\n")
+    print("ha vivido en:")
+    try:
+        
+        for i in range(2,10):
+            print(driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[{}]/div/div/div[2]/div[2]/div/span/span".format(i)).text)
+            ele=driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[{}]/div/div/div[2]/div[1]/span/a/span/span'.format(i)).text
+            print(ele)
+            print("\n")
+            
+    except:
+        print(("No hay más sitios"))
+        
+def info_contact(driver):
+    time.sleep(2)
+    driver.get("https://www.facebook.com/joaquincasado14/about_contact_and_basic_info")
+    print("\n")
+    print("Información de contacto:")
+    print(driver.find_element(By.CSS_SELECTOR, ".x1gan7if:nth-child(2) .x9f619 > .x9f619 > .x193iq5w").text)
+    print(driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[2]/div/div/div[2]/div/div/div/div/div[1]/span").text)
+    try:
+        for i in range(1,5):
+            print(driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[2]/div/div/div[2]/div[{}]/div/div/div/div[1]/span".format(i)).text)
+    except:
+        print("nada mas")
+        
     
     
 def login(email, password):
     """ Logging into our own profile """
-   
-    try:
-        global driver
-        options = Options()
+    global driver
+    options = Options()
         #  Code to disable notifications pop up of Chrome Browser
-        options.add_argument("--disable-notifications")
-        options.add_argument("--disable-infobars")
-        options.add_argument("--mute-audio")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--mute-audio")
         # options.add_argument("headless")
-        try:
-            driver = webdriver.Chrome(executable_path=r"C:\Users\zorro\AppData\Local\Programs\Python\Python310\Lib\site-packages\selenium\chromedriver.exe", options=options)
-            print("you logged in. Let's rock")
-        except:
-            print("you need web driver!")
-            exit()
-
+    try:
+        driver = webdriver.Chrome(executable_path=r"C:\Users\zorro\AppData\Local\Programs\Python\Python310\Lib\site-packages\selenium\chromedriver.exe", options=options)
+        print("you logged in. Let's rock")
+    except:
+        print("you need web driver!")
+        exit()
         #driver.get("https://en-gb.facebook.com")
-        driver.get("https://facebook.com")
-        
+    driver.get("https://facebook.com")       
         #driver.maximize_window()
         # filling the form
-
-        driver.find_element(By.CSS_SELECTOR,"._9xo5 ._9xo7").click()
-        driver.find_element(By.XPATH, "//input[@placeholder='Correo electrónico o número de teléfono']").send_keys(email)
-        driver.find_element(By.XPATH, "//input[@placeholder='Contraseña']").send_keys(password)
+    driver.find_element(By.CSS_SELECTOR,"._9xo5 ._9xo7").click()
+    driver.find_element(By.XPATH, "//input[@placeholder='Correo electrónico o número de teléfono']").send_keys(email)
+    driver.find_element(By.XPATH, "//input[@placeholder='Contraseña']").send_keys(password)
         # clicking on login button
-
-        driver.find_element(By.NAME,"login").click()
-        
-        #buscarPersona(driver)
-        #busca_amigos(driver)--ya funciona
-        #busca_fotos(driver)--casi funciona
-        #busca_info(driver)--falta completar
-        
-    except:
-        print("maybe something is wrong")
+    driver.find_element(By.NAME,"login").click()
+    #buscarPersona(driver)
+    #busca_amigos(driver)
+    #busca_fotos(driver)--casi funciona
+    #busca_info(driver)
+    #info_work_education(driver)
+    #info_places_lived(driver)
+    info_contact(driver) 
+    
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
 def main():
-    
     with open('credentials.txt') as f:
         email = f.readline().split('"')[1]
         password = f.readline().split('"')[1]
@@ -143,8 +203,6 @@ def main():
        # driver.close()
     else:
         print("Input file is empty.")
-
-
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # -------------------------------------------------------------
